decbb62f65026a27939f527583294e5c6e3fd705
thecroshel <burak21efee@gmail.com>
2024-08-12 13:59:48 +0300

    Add files via upload

---
+++ JoinLogger/EventHandlers.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.Events.EventArgs;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace JoinLogger
+{
+    public class EventHandlers
+    {
+        public void OnPlayerJoined(JoinedEventArgs ev)
+        {
+            string steam64Id = ev.Player.UserId.Replace("@steam", "");
+            Log.Info($"{ev.Player.Nickname} ({steam64Id}) has joined the server.");
+        }
+
+        public void OnPlayerLeft(LeftEventArgs ev)
+        {
+            string steam64Id = ev.Player.UserId.Replace("@steam", "");
+            Log.Info($"{ev.Player.Nickname} ({steam64Id}) has left the server.");
+        }
+    }
+}
---
+++ JoinLogger/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace JoinLogger
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "JoinLogger";
+        public override string Author => "thecroshel";
+        public override Version RequiredExiledVersion => new Version(8, 4, 3); // This requires the System namespace
+        public override string Prefix => "JoinLogger";
+
+        public EventHandlers EventHandlers;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            EventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Player.Joined += EventHandlers.OnPlayerJoined;
+            Exiled.Events.Handlers.Player.Left += EventHandlers.OnPlayerLeft;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Player.Joined -= EventHandlers.OnPlayerJoined;
+            Exiled.Events.Handlers.Player.Left -= EventHandlers.OnPlayerLeft;
+            EventHandlers = null;
+        }
+    }
+}
